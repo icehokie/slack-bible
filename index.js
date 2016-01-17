7fed867025aded9ac756a467cea910e133d33251
Justin Bolling <justin.bolling@gmail.com>
2016-01-17 14:25:32 -0500

    Added support for multiple versions and inline text response

--- index.js
+++ index.js
@@ -2,47 +2,85 @@ var _ = require('lodash')
 var bodyParser = require('body-parser')
 var books = require('./books')
 var express = require('express')
+var versions = require('./versions')
 var app = express()
+var http = require('follow-redirects').http
+var async = require('async')
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 var booksWithPipes = _.chain(books).values().flatten().join('|').value()
-var regex = new RegExp('(' + booksWithPipes + ')\\.?\\s*(\\d{1,3})(?:\\s*[\\:\\s]\\s*(\\d{1,3})(?:\\s*[\\-\\s]\\s*(\\d{1,3}))?)?', 'ig')
+var versionsWithPipes = _.chain(versions).values().flatten().join('|').value()
+var regex = new RegExp('(' + booksWithPipes + ')\\.?\\s*(\\d{1,3})(?:\\s*[\\:\\s]\\s*(\\d{1,3})(?:\\s*[\\-\\s]\\s*(\\d{1,3}))?)?\\s*('+versionsWithPipes+')?','ig')
+var textParser = new RegExp('og:description\" content=\"(.*)\"/>','i')
 
-function reverseLookup (abbr) {
+function reverseLookupBook (abbr) {
   return _.findKey(books, function (abbrs) {return _.includes(abbrs, abbr.toLowerCase())})
 }
 
-app.post('/', function(request, response) {
-  var inputText = request.body.text
+function reverseLookupTranslation (abbr) {
+  return _.findKey(versions, function (abbrs) {return _.includes(abbrs, abbr.toLowerCase())})
+}
 
+app.post('/', function(request, response) {
+  async.series([function(callback) {
+  var inputText = request.body.text
   var res, results = []
   while ((res = regex.exec(inputText)) !== null) {
-    var trueRef = reverseLookup(res[1]) + '+' + res[2]
+	var version = '';
+    var trueRef = reverseLookupBook(res[1]) + '+' + res[2]
     if (res[3]) trueRef += ':' + res[3]
     if (res[4]) trueRef += '-' + res[4]
+	if (res[5]) version = reverseLookupTranslation(res[5])
+	
+	if (!version)
+		version = 'ESV'
+	
+	trueRef += '&version='+version
     results.push(trueRef)
   }
 
   if (results.length > 0) {
     var attachments = _.map(results, function (result) {
       var title = result.replace('+', ' ')
+	  title = title.replace('&version=', ' - ')
       var link = 'https://biblegateway.com/bible?passage=' + result
+	  var strText = ''
+	   var options = {
+			host: 'www.biblegateway.com',
+			path: '/passage/?search='+result
+			};
 
-      return {
+		http.get(options, function(res) {
+			var bodyChunks= '';
+		res.on("data", function(chunk) {
+			bodyChunks += chunk;
+			
+		}).on("end", function() {
+			var verse = textParser.exec(bodyChunks);
+			if (verse && verse[1]) strText = verse[1]
+			callback(false, {title:title, title_link: link, fallback: title + ' - ' + link, color: 'good', text: link+'\n*'+title+'*\n>'+strText})
+		}) ;
+		
+		});
+	  
+    /**  return {
         title: title,
         title_link: link,
         fallback: title + ' - ' + link,
-        color: 'good'
-      }
+        color: 'good',
+		text: strText
+      }*/
     })
-
-    response.json({text: 'Referenced verses:', attachments: attachments})
   } else {
     response.json({})
   }
+  }],
+  function (err, results) {
+	  response.json(results[0])
+  })
 })
 
 app.listen(app.get('port'), function() {
